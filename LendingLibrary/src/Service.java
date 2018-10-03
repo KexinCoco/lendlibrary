4e870195409e644cc177d8b4ceacfb831f499b72
KexinYu1 <37640347+KexinYu1@users.noreply.github.com>
2018-10-04 06:07:19 +0800

    users' choice

--- LendingLibrary/src/Service.java
+++ LendingLibrary/src/Service.java
@@ -5,8 +5,10 @@ public class Service
 		public static void main(String[] args) {
 			Scanner ask = new Scanner(System.in);
 			System.out.println("Which genre of books do you like? Mystrey, Love, Science Fiction or Fantasy?");
-			String anser = ask.nextLine(); 
-			  
+			String answer = ask.nextLine(); 
+			if (answer=="mystery") {
+				System.out.println("We have two choices.");
+			}
 		}
 			
             
