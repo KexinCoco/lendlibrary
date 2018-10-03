4ee74e453ade96b19dc84d5c98b80e4952e159f3
kexin.yu <kexin.yu@MU-C1-19.mullen.pvt.k12.co.us>
2018-10-03 14:44:51 -0600

    second

--- LendingLibrary/src/Database.java
+++ LendingLibrary/src/Database.java
@@ -3,14 +3,22 @@
 import java.util.ArrayList;
 
 
+
+
 public class Database{
 	public static ArrayList <Book> name = new ArrayList <Book> ();
 
 	
 {
-	
-	
-	
+
+	name.add(new Book("Paper Town", "","Mystery") );
+	name.add(new Book("The Magpie Murders", " Anthony Horowitz","Mystery") );
+	name.add(new Book("Harry Poter", "J.K. Rowling","Fantasy") );
+	name.add(new Book("The Lion, the Witch and the Wardrobe", "C. S. Lewis","Fantasy") );
+	name.add(new Book("Jane Eyre", "Charlotte Brontë","Love") );
+	name.add(new Book("Pride and Prejudice" , "Jane Austen","Love") );
+	name.add(new Book("The Hunger Games", "Suzanne Collins","Science fiction") );
+	name.add(new Book("The Time Machine", "H. G. Wells", "Science fiction") );
 	
 }
 }
--- LendingLibrary/src/Service.java
+++ LendingLibrary/src/Service.java
@@ -2,11 +2,9 @@
 public class Service
 	{
 		public static void main(String[] args) {
-			
-			System.out.println("Which genre do you like? Mystery, Fiction, Science Fiction or Fantasy");
+			System.out.println("Which genre do you like? Mystery, Love, Science Fiction or Fantasy");
 			
 			
 		
 		}
-		
 	}
